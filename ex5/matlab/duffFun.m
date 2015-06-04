3750a1964b355780caaf45230da33ab3a6c9e250
v0lta <moritzwolter@gmx.de>
2015-06-04 15:20:48 +0200

    duffing ok.

--- ex5/matlab/duffFun.m
+++ ex5/matlab/duffFun.m
@@ -4,8 +4,20 @@ function [ dx ] = duffFun( t,x )
 
 dx = zeros(2,1);
 
-k = 0.2;
-B = 12;
+%chaos unique n=2 pair
+%B = 7.5;
+%k = 0.05;
+%k = 0.2;
+%k = 0.185;
+
+%chaos unique
+%B = 11.5;
+%k = 0.2
+%k = 0.185;
+
+%no chaos;
+k = 1;
+B = 5;
 
 dx(1) = x(2);
 dx(2) = -k*x(2) - x(1)^3 + B * cos(t);
--- ex5/matlab/duffing.m
+++ ex5/matlab/duffing.m
@@ -1,5 +1,59 @@
+%% compute the data.
+tend = 20;
+[T1,X1] = ode45(@duffFun,[0 tend],[2.0 0]);
+[T2,X2] = ode45(@duffFun,[0 tend],[2.1 0]);
+[T3,X3] = ode45(@duffFun,[0 tend],[2.2 0]);
+[T4,X4] = ode45(@duffFun,[0 tend],[2.3 0]);
+
+%arrange some nice colors.
+figure(1)
+colorMat = lines;
+color1 = colorMat(1,:);
+color2 = colorMat(2,:);
+color3 = colorMat(3,:);
+color4 = colorMat(4,:);
+
+plot(X1(:,1),X1(:,2),'color',color1)
+hold on
+plot(X2(:,1),X2(:,2),'color',color2)
+plot(X3(:,1),X3(:,2),'color',color3)
+plot(X4(:,1),X4(:,2),'color',color4)
+
+plot(X1(end,1),X1(end,2),'o','color',color1)
+plot(X2(end,1),X2(end,2),'o','color',color2)
+plot(X3(end,1),X3(end,2),'o','color',color3)
+plot(X4(end,1),X4(end,2),'o','color',color4)
+hold off;
+
+figure(2)
+plot(T1,X1(:,1),'color',color1)
+plot(T1,X1(:,2),'color',color1)
+hold on
+plot(T2,X2(:,1),'color',color2)
+plot(T2,X2(:,2),'color',color2)
+plot(T3,X3(:,1),'color',color3)
+plot(T3,X3(:,2),'color',color3)
+plot(T4,X4(:,1),'color',color4)
+plot(T4,X4(:,2),'color',color4)
 
 
-[T,X] = ode45(@duffFun,[0 200],[0 0]);
-
-plot(X(:,1),X(:,2))
+% %% make a movie
+% writerObj = VideoWriter('duffing.avi');
+% open(writerObj);
+% 
+% axis tight
+% set(gca,'nextplot','replacechildren');
+% set(gcf,'Renderer','zbuffer');
+% 
+% for k = 1:(length(X4))
+%    plot(X1(k,1),X1(k,2),'o','color',color1)
+%    hold on;
+%    plot(X2(k,1),X2(k,2),'o','color',color2)
+%    plot(X3(k,1),X3(k,2),'o','color',color3)
+%    plot(X4(k,1),X4(k,2),'o','color',color4)
+%    frame = getframe;
+%    writeVideo(writerObj,frame);
+% end
+% hold off;
+% 
+% close(writerObj);
